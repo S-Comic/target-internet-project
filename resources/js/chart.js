cca5a78816a85880864a5a0ef809cb50b943098f
Samuel Jule <samjule@outlook.com>
2021-07-24 04:19:08 +0100

    Adds Chart.Js. Boilerplates end screen.

---
+++ resources/js/chart.js
@@ -0,0 +1,1 @@
+/* Will be used to hold chart code */
---
+++ resources/views/index.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Target Internet Project</title>
+
+        <!-- Local Scripts / Styles -->
+        <script src="/js/app.js"></script>
+        <link href="/css/app.css" rel="stylesheet">
+
+        <!-- CDNs -->
+        <!-- Move chart.js from CDN to local when finished testing -->
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet">
+        
+    </head>
+
+    <body class="antialiased">
+
+    <!-- Include the ouput blade template -->
+    @include('components.output') 
+
+    </body>
+</html>
